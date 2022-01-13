381a8eee901f482391617549d08e05ce5053d0ec
Michael Chong <mikec0920@gmail.com>
2022-01-12 16:19:59 -0800

    Completed exercises

--- 0_select_basics.sql
+++ 0_select_basics.sql
@@ -1,3 +1,5 @@
+-- https://sqlzoo.net/wiki/SELECT_basics
+
 -- 1. The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
 -- Modify it to show the population of Germany
 SELECT population FROM world
